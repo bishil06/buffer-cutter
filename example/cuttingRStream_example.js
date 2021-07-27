5c2c0ccb7760c84ebcb020e542920cc723edcda2
bishil06 <36038548+bishil06@users.noreply.github.com>
2021-07-28 06:01:42 +0900

    fix bufferCutter
    fix cuttingRStream

--- example/cuttingRStream_example.js
+++ example/cuttingRStream_example.js
@@ -6,7 +6,7 @@ const buffer = Buffer.from('abcdefghi');
 // <Buffer 61 62 63 64 65 66 67 68 69>
 const bufStream = Readable.from(buffer);
 // readalbe
-for await (const c of cuttingRStream(bufStream, { length: 3 })) {
+for await (const c of cuttingRStream(bufStream, { length: 2 })) {
   console.log(c);
 }
 // <Buffer 61 62 63>
