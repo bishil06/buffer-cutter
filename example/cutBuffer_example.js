afcc95dc40201a5b32e19f1d15b1886298a1a2b9
bishil06 <36038548+bishil06@users.noreply.github.com>
2021-07-28 18:12:23 +0900

    feat bufferCutter
    add check arguments

--- example/cutBuffer_example.js
+++ example/cutBuffer_example.js
@@ -5,3 +5,7 @@ const buffer = Buffer.from('abcde');
 let r = cutBuffer(buffer, { deepCopy: true, start: 0, end: 3 });
 console.log(r);
 // <Buffer 61 62 63>
+
+r = cutBuffer(buffer);
+console.log(r);
+// <Buffer 61 62 63 64 65>
--- example/cuttingBuffer_example.js
+++ example/cuttingBuffer_example.js
@@ -5,3 +5,6 @@ const buffer = Buffer.from('abcde');
 let r = cuttingBuffer(buffer, { deepCopy: true, length: 2 });
 console.log(r);
 // [ <Buffer 61 62>, <Buffer 63 64>, <Buffer 65> ]
+r = cuttingBuffer(buffer);
+console.log(r);
+// [ <Buffer 61 62 63 64 65> ]
